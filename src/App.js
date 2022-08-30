8e125575fabbc9d107c2d53dc03e4f9d85adceb6
Sepideh Attar <sepideh.att@gmail.com>
2022-08-31 00:22:09 +0430

    imported navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useState,useEffect } from "react";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 
 import LogIn from "./pages/LogIn";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home isLoggedIn={isLoggedIn} />}></Route>
         <Route path='/auth/login' element={<LogIn setIsLoggedIn={setLoggedIn} />} />
