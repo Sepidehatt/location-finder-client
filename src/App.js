ec0f7a6a171225a01dbf357d11657d7d8275b529
Sepideh Attar <sepideh.att@gmail.com>
2022-08-30 10:17:56 +0430

    saved coords to DB, error handeling getting coords

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const getLocation = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(getCoordinates)
+      navigator.geolocation.getCurrentPosition(getCoordinates, showError)
     } else {
       alert('Geolocation is not supported by this browser')
     }
@@ -21,12 +21,42 @@ function App() {
   }
 
 
-  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    getLocation()
+    if (latitude !== null && longitude !== null) {
+      axios
+        .post("http://localhost:3001/create-location", { latitude, langitude: longitude })
+        .then(response => {
+          console.log(response.data)
+        })
+    }
+  }
+
+  function showError(error) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        alert("User denied the request for Geolocation.")
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.")
+        break;
+      case error.TIMEOUT:
+        alert("The request to get user location timed out.")
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.")
+        break;
+      default:
+        alert("An unknown error occurred.")
+
+    }
+  }
 
 
   return (
     <div className="App">
-      <form >
+      <form onSubmit={handleSubmit}>
         <button onClick={getLocation}>Get Coordinate</button>
       </form>
       <h4>Latitude : {latitude}</h4>
